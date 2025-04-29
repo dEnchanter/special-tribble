d7c9a3db1010565a7a3318b16cc02a119d89f612
dEnchanter <afolabiopaleye@gmail.com>
2025-04-29 20:14:45 +0100

    ...

--- src/types/dashboard.d.ts
+++ src/types/dashboard.d.ts
@@ -2,6 +2,6 @@ interface Dashboard {
   itemCode: string;      // Item ID (string)
   requester: string;     // Request By (string)
   approver: string;      // Approved By (string)
-  quantity: number;      // Quantity (number)
+  quantity: string;      // Quantity (number)
   createdAt: string;     // Date Created (ISO string format)
 }
